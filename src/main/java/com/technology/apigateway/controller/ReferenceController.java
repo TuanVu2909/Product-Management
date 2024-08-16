0f327d532c294c3d395726de8dde84fab1acd960
lamlt1211 <lamchuot0@gmail.com>
2024-08-16 10:05:55 +0700

    ok

--- src/main/java/com/technology/apigateway/controller/ReferenceController.java
+++ src/main/java/com/technology/apigateway/controller/ReferenceController.java
@@ -8,8 +8,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
