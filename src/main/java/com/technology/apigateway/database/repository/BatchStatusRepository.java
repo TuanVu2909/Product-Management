5599f91c763f33cb7234c977d40b90e12bd03fdf
lamlt1211 <lamchuot0@gmail.com>
2024-09-25 17:11:56 +0700

    commit

--- src/main/java/com/technology/apigateway/database/repository/BatchStatusRepository.java
+++ src/main/java/com/technology/apigateway/database/repository/BatchStatusRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository("batchStatusRepository")
-public interface BatchStatusRepository extends CrudRepository<BatchStatus, Integer> {
+public interface BatchStatusRepository extends CrudRepository<BatchStatus, String> {
 
-    @Procedure("prc_runbatch")
+    @Procedure("BatchStatus.prc_runbatch")
     BatchStatus runBatch(@Param("pv_userId") Integer userId);
 }
