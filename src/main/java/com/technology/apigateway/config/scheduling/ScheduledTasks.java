c4ee1bf8d54fe38755ab616ea903099a38b63161
Le Cao Son <126761354+TuanVu2909@users.noreply.github.com>
2024-10-30 14:14:41 +0700

    scheduled

--- src/main/java/com/technology/apigateway/config/scheduling/ScheduledTasks.java
+++ src/main/java/com/technology/apigateway/config/scheduling/ScheduledTasks.java
@@ -3,8 +3,12 @@ package com.technology.apigateway.config.scheduling;
 import com.technology.apigateway.service.impl.SMSSendingServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Configuration
+@EnableScheduling
 public class ScheduledTasks {
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
